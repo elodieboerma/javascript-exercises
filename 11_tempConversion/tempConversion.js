00dccfb8496623da60b399c392b56457fa3eaf80
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2025-11-05 22:31:02 -0600

    do exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+// mine works too, but the solution is more compact because it's only one line inside each function
+//return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+//return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+
+const convertToCelsius = function(tempFahr) {
+  let tempCels = (tempFahr - 32) * (5/9);
+  return tempCels.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCels) {
+  let tempFahr = tempCels * (9/5) + 32;
+  return tempFahr.toFixed(1);
 };
+convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
