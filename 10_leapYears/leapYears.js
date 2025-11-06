115ac4dc46df82653656757ed47fc4b9e346adf0
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2025-11-05 22:02:31 -0600

    do exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // I think I did it right, but the solution created constants to improve readbility.
+    if(year % 4 === 0 && !(year % 100 === 0)) {
+        return true;
+    }else if(year % 100 === 0 && year % 400 === 0){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
