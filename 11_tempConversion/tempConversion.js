7d2b7a71378ce01443c07034c6eba17e85fa2d29
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2025-11-05 22:38:28 -0600

    explain differences between my answer & solution

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,4 +1,7 @@
-// mine works too, but the solution is more compact because it's only one line inside each function
+// mine works too, but:
+  // the solution is more compact because it's only one line inside each function
+  // toFixed() returns a string, so must use parseFloat() to turn it back into a number
+  // and always returns a decimal place even if it's a 0
 //return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 //return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 
