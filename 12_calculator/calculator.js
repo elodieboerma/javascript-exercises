28662f2964850ce00622ba050c2fa00e518a3b88
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2025-12-25 22:15:10 -0600

    do exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;  
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total,currValue) => 
+    total + currValue,0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total,currValue) =>
+    total * currValue,1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	//`factorial()`: given a single number, find its [factorial]
+  /*const final = num;
+  while (num > 1) {
+    num--;
+    final = final * num;
+  }
+  return final;*/
+  let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
