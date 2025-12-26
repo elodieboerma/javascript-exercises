75fd5c8637a777df9679aa64f6be5d6c4354e4e7
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2025-12-25 23:46:12 -0600

    do exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const validChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const array = string
+        .toLowerCase()
+        .split("")
+        .filter(char => validChars.includes(char))
+        .join("");
+    
+    const reversed = array
+        .split('')
+        .reverse()
+        .join("");
+    
+    return array === reversed;
+
+    /*const cleanedString = string
+    .toLowerCase()
+    .split('')
+    .filter((character) => alphanumerical.includes(character))
+    .join('');
+
+  // Create a new reversed string for comparison
+  const reversedString = cleanedString.split('').reverse().join('');
+
+  // Return the outcome of the comparison which will either be true or false
+  return cleanedString === reversedString;*/
 };
 
 // Do not edit below this line
