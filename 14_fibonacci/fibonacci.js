ab731c86d7215298103f1f03417f09399dc4254a
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2025-12-26 00:26:29 -0600

    do exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+
+    if (n < 0) {return "OOPS"};
+    if (n === 0) {return 0};
+    if (n === 1) {return 1};
+    
+    let num1 = 1;
+    let num2 = 1;
+    for (let i = 3; i <= n; i++) {
+            let sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
+    return num2;
 
 };
 
