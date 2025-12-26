6a07e9074050ba4fb7cca040548db525b896499d
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2025-12-26 00:55:25 -0600

    do exercise 16
    -kinda copied and pasted a lot from solution, but I think I understand how it works now

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest,currPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currAge = getAge(currPerson.yearOfBirth, currPerson.yearOfDeath);
+        return oldestAge < currAge ? currPerson : oldest;
+    });
 };
 
 // Do not edit below this line
