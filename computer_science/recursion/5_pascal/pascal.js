9cb9131a4918510ff2c092568ef033cf486be180
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-06-24 21:59:09 -0500

    do exercise 5

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,16 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+    let currentRow = [1];
+
+    if (n === 1) { return currentRow };
+
+    const prevRow = pascal(n - 1);
+
+    prevRow.forEach((number, i) => {
+        const rightNeighbor = prevRow[i + 1] ?? 0;
+        currentRow.push(number + rightNeighbor);
+    })
+
+  return currentRow;
 };
   
 // Do not edit below this line
