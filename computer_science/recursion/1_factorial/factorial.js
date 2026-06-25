5db7033dd1d6b8b3f8dfd276963ca5f52adebd54
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-06-24 19:52:23 -0500

    do exercise 1

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,10 @@
-const factorial = function() {
-
+const factorial = function(int) {
+    if (int === 0) {
+        return 1;
+    } else if (int < 0 || int % 1 !== 0 || typeof int !== 'number') {
+        return undefined;
+    }
+    return int * factorial(int - 1);
 };
 
 // Do not edit below this line
