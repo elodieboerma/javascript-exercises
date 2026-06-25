871fe7ef6fa956f7909579d24f4165dbce244147
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-06-24 20:57:15 -0500

    do exercise 2

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,11 @@
-const contains = function() {
-  
+const contains = function(object,value) {
+    const values = Object.values(object);
+
+    if (values.includes(value)) return true;
+
+    const nestedObjects = values.filter(value => typeof value === 'object' && value !== null);
+
+    return nestedObjects.some(nestedObject => contains(nestedObject, value));
 };
   
 // Do not edit below this line
